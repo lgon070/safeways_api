2c3d4c4eabb1888b8efb2bbf291bf206b110d575
Luis <39094995+lgon070@users.noreply.github.com>
2021-01-28 17:20:29 -0800

    remove api key
    good one! that was the longest streak

--- util.py
+++ util.py
@@ -198,7 +198,6 @@ def get_accidents(lat: float, lng: float, radius: float, accidents: List[dict]) 
 
 
 def find_directions(origin: LatLng, destination: LatLng, method: str) -> list:
-    # https://maps.googleapis.com/maps/api/directions/json?origin=34.053989,-118.243217&destination=34.059521,-118.274872&mode=walking&alternatives=true&key=AIzaSyDrGUoICmyI28kv6leNximWqI_HqZRMeEU
     parameters = {
         "origin": f'{origin[0]},{origin[1]}',
         "destination": f'{destination[0]},{destination[1]}',
